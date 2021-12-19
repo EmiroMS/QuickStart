bff7d00481055c10b855c46e002aa945453d9037
EmiroMS <55450818+EmiroMS@users.noreply.github.com>
2021-12-18 21:04:02 -0500

    terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,22 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
  
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -27,16 +33,10 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent
   ],
   bootstrap: [
     AppComponent
   ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,20 +1,16 @@
 import { Product } from './products';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 export class CartService {
   items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
 /* . . . */
-
-  addToCart(product: Product) {
-    this.items.push(product);
-  }
-
-  getItems() {
-    return this.items;
-  }
-
-  clearCart() {
-    this.items = [];
-    return this.items;
-  }
-/* . . . */
+getShippingPrices() {
+  return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+}
 }
